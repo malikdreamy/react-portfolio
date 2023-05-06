5c641e5b636fe43e74c9ed821e8d0d78116822dc
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-05 23:38:13 -0700

    Update Bio.js
    adding styles to Bio.js

--- src/Bio.js
+++ src/Bio.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default function Bio() {
     return (
       <>
-       <header className="text-center mb-5">
-      <h1>Malik Tornes</h1>
-      <p>Website Programmer</p>
+  
+       <header className="text-center mb-5 bg-danger" >
+      <h1 style={{color: 'white'}}>Malik Tornes</h1>
+      <p style={{color: 'white'}}>JavaScript Programmer</p>
     </header>
-    <section id="aboutme" className="mb-5">
+    <section id="aboutme" className="mb-5 bg-dark">
     <div className="row">
     <div className="col-md-4">
  
@@ -15,7 +16,7 @@ export default function Bio() {
     </div>
     <div className="col-md-8">
       <h2>About me</h2>
-      <p className="lead">Hi, my name is Malik Tornes. I am 26 years old and I currently live in Woodland Hills, CA. I am a full stack developer with an advanced skillset in HTML, CSS, and JavaScript. Below you will see a few of my latest projects.</p>
+      <p className="lead" style={{color: 'white'}}><strong>Hi, my name is Malik Tornes. I am 26 years old and I currently live in Woodland Hills, CA. I am a full stack developer with an advanced skillset in HTML, CSS, and JavaScript. Below you will see a few of my latest projects.</strong></p>
     </div>
     </div>
     </section>
