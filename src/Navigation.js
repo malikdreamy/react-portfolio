7aa74ce1d25de0317c7bae22083b7039252168c8
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-05 23:41:17 -0700

    Update Navigation.js
    Added state to Resume

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,8 @@
 import React, {useState} from 'react'
 import Bio from './Bio'
-
+import Resume from './Resume'
 import Projects from './Projects'
 import Contact from './Contact'
-
-
-
 
 export default function Navigation() {
 let [page, setPage] = useState(Bio)
@@ -22,33 +19,41 @@ function showAboutMe(){
   setPage(page = Bio)
 }
 
+function viewResume(){
+  setPage(page = Resume)
+}
+
+
 
 
     return (
       <>
       
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-danger" >
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">My Portfolio</a>
+      <a className="navbar-brand" href="#" style={{color: 'white'}}><strong>Portfolio</strong></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ms-auto">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#aboutme" onClick={showAboutMe}>ABOUT ME</a>
+            <a className="nav-link active" aria-current="page" href="#aboutme" onClick={showAboutMe} style={{color: 'white'}}><strong>ABOUT ME</strong></a>
+          </li>
+          <li className="nav-item" >
+            <a className="nav-link" href="#projects" onClick={showProjects} style={{color: 'white'}}><strong>Projects</strong></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#projects" onClick={showProjects}>Projects</a>
+            <a className="nav-link" href="#contact" onClick={contactMe} style={{color: 'white'}}><strong>Contact Me</strong></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#contact" onClick={contactMe}>Contact Me</a>
+            <a className="nav-link" href="#contact" onClick={viewResume} style={{color: 'white'}}><strong>Resume</strong></a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
-  <div>{page}</div>
+  <div className="bg-dark">{page}</div>
 
   </>
     
