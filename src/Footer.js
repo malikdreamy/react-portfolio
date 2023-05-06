ad73a2f85c881a82cd321760b65b42cd6fcd7f55
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-05 23:40:25 -0700

    Update Footer.js
    Adding Color to Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function Bio() {
+export default function Footer() {
     return (
       <>
 
-<footer className="text-center mt-5">
-  <p>&copy; 2023 Malik Tornes</p>
+<footer className="text-center mt-5 bg-dark">
+  <p style={{color: 'white'}}>&copy; 2023 Malik Tornes</p>
   <a href="https://github.com/malikdreamy">Malik's GitHub</a>
 </footer>
   
       </>
     );
   }
+  
   
